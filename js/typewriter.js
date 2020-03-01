7003748e141bfcc844d176e758b937503576125b
Excaliburns <tutsandallthat@gmail.com>
2020-03-01 00:07:13 -0500

    added links to other pages
    	modified:   css/index.css
    	modified:   index.html
    	modified:   js/typewriter.js
    	new file:   landing.html
    	new file:   league.html

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,11 +1,12 @@
 var text = $('.typewriter').text();
 
 var length = text.length;
+var timeout;
 var character = 0;
 
 
-setTimeout( () => {
-    $( document ).ready( function () {
+setTimeout(() => {
+    $(document).ready(function () {
         $('.typewriter').text("");
         $('.typewriter').css('visibility', 'visible')
     });
@@ -13,11 +14,22 @@ setTimeout( () => {
 
     (function typeWriter() {
 
-        setTimeout(function () {
+        timeout = setTimeout(function () {
             character++;
             var type = text.substring(0, character);
             $('.typewriter').text(type);
             typeWriter();
+
+
+
+            // stop the animation
+            if (length == character) {
+                clearTimeout(timeout);
+            }
+
         }, 25);
+
     }())
+
+
 }, 3000);
