e4470766040519d81d5a61c7492c28015c269da4
Excaliburns <tutsandallthat@gmail.com>
2020-07-27 06:37:45 -0400

    modified:   css/index.css 	modified:   css/portfolio.css 	modified:   css/resume.css 	new file:   img/Discord_yZ8XG8o7QP.png 	new file:   img/ZahzRql.png 	new file:   img/chrome_0u13X5lPR0.png 	new file:   img/qdk4zFM.png 	modified:   js/resume.js 	modified:   resume/portfolio.html 	modified:   resume/resume.html

--- js/resume.js
+++ js/resume.js
@@ -6,23 +6,6 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     }, 500);
 });
 
-
-$(document).on("scroll", function () {
-    var pageTop = $(document).scrollTop();
-    var pageBottom = pageTop + $(window).height();
-    var tags = $("section")
-
-    for (var i = 0; i < tags.length; i++) {
-        var tag = tags[i] 
-    
-        if ($(tag).position().top < pageBottom) { 
-            $(tag).addClass("fadeInSection")
-        } else {
-            $(tag).removeClass("fadeInSection")
-        }
-    }
-});
-
 $(document).on('click', '#skill-dots-toggle', function (event) {
     $('.skill-dots').slideToggle();
 });
